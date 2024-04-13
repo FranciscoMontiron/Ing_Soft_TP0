f9cee07753a2921fa5520cfc8ed6fe952e393c99
Francisco Montiron <franciscomontiron@gmail.com>
2024-04-13 15:13:09 -0300

    Modifico el html detail para que sea mas visible

--- TP0/polls/views.py
+++ TP0/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+# from django.http import Http404
 from django.http import HttpResponse
 # from django.template import loader
 
@@ -37,9 +37,13 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+# def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, "polls/detail.html", {"question": question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
